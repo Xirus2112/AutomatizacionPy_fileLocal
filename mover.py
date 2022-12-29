670d308beab5043ff24f0c7858f8720b34206060
Carlos Alvarado M <66229925+Xirus2112@users.noreply.github.com>
2022-12-29 15:29:59 -0500

    se cambian las rutas de extraccion y envio de archivo

--- mover.py
+++ mover.py
@@ -1,21 +1,21 @@
 from PIL import Image
 import os
 
-downloadsFolder = "/Users/nicolasschurmann/Downloads/"
-picturesFolder = "/Users/nicolasschurmann/Pictures/"
+downloadsFolder = "D:/Users/carlos.alvarado/Downloads/"
+picturesFolder = "D:/Users/carlos.alvarado/Downloads/"
 
 if __name__ == "__main__":
     for filename in os.listdir(downloadsFolder):
         name, extension = os.path.splitext(downloadsFolder + filename)
 
-        if extension in [".jpg", ".jpeg", ".png"]:
-            picture = Image.open(downloadsFolder + filename)
-            picture.save(picturesFolder + "compressed_"+filename, optimize=True, quality=60)
-            os.remove(downloadsFolder + filename)
-            print(name + ": " + extension)
+        # if extension in [".jpg", ".jpeg", ".png"]:
+        #     picture = Image.open(downloadsFolder + filename)
+        #     picture.save(picturesFolder + "compressed_"+filename, optimize=True, quality=60)
+        #     os.remove(downloadsFolder + filename)
+        #     print(name + ": " + extension)
 
         if extension in [".mp3"]:
-            musicFolder = "/Users/nicolasschurmann/Music/"
+            musicFolder = "D:/Users/carlos.alvarado/Music/"
             os.rename(downloadsFolder + filename, musicFolder + filename)
 
 
